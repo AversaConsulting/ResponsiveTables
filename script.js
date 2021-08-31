13d72f146e17d418bcd4287fbdcfba33661629ec
Aversa Consulting <nick@aversa.co.uk>
2021-08-31 11:14:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-
+ function setupResponsiveTables() {
+        var tables = document.getElementsByClassName("av-table-responsive");
+        for (t = 0; t < tables.length; t++) {
+            var table = tables[t];
+            var tablerows = table.getElementsByTagName("tr");
+            var tableheads = tablerows[0].getElementsByTagName("th");
+            
+            if (tableheads.length>0) {
+                for (r = 1; r < tablerows.length; r++) {
+                    var row = tablerows[r];
+                    var tablecols = row.getElementsByTagName("td");
+                    for (c = 0; c < tablecols.length; c++) {
+                        var col = tablecols[c];
+                        if (!col.hasAttribute("data-label")) {
+                            var head = tableheads[c];
+                            if (head.hasAttribute("data-label")) {
+                                col.setAttribute("data-label", head.getAttribute("data-label"));
+                            } else {
+                                col.setAttribute("data-label", head.innerHTML);
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
